31ae70e90652676c4f2d32652469d1400ac6411f
Raphael <raphaelrpais@gmail.com>
2018-01-16 18:31:00 -0200

    menu principal
    funcionando na gambiarra maligna

--- scripts.js
+++ scripts.js
@@ -9,24 +9,64 @@ $(function() {
     
 });
 
-$('.ed-home__button--menu--text').mouseenter(
+//Hover do Menu... Sim, isso tudo.
+
+$('.ed-home__button--menu.ed-cyan').mouseenter(
     function(){
-        $(this).addClass('animated fadeOutDown');
+        $('.ed-cyan > .ed-home__button--menu--text').addClass('animated fadeOutDown');
+        $('.ed-cyan > .ed-home__button--menu--hover').addClass('animated fadeInUp');
+        $('.ed-cyan > .ed-home__button--menu--hover').removeClass('fadeOutDown');
 }); 
 
-$('.ed-home__button--menu--text').mouseleave(
+$('.ed-home__button--menu.ed-cyan').mouseleave(
     function(){
-        $(this).removeClass('animated fadeOutDown')
-        $(this).addClass('animated fadeInUp');
+        $('.ed-cyan > .ed-home__button--menu--text').removeClass('animated fadeOutDown');
+        $('.ed-cyan > .ed-home__button--menu--text').addClass('animated fadeInUp');
+        $('.ed-cyan > .ed-home__button--menu--hover').removeClass('animated fadeInUp');
+        $('.ed-cyan > .ed-home__button--menu--hover').addClass('animated fadeOutDown');
 });
 
-$('.ed-home__button--menu--hover').mouseenter(
+$('.ed-home__button--menu.ed-magenta').mouseenter(
     function(){
-        $(this).addClass('animated fadeInUp ed-appear');
+        $('.ed-magenta > .ed-home__button--menu--text').addClass('animated fadeOutDown');
+        $('.ed-magenta > .ed-home__button--menu--hover').addClass('animated fadeInUp');
+        $('.ed-magenta > .ed-home__button--menu--hover').removeClass('fadeOutDown');
 }); 
 
-$('.ed-home__button--menu--hover').mouseleave(
+$('.ed-home__button--menu.ed-magenta').mouseleave(
     function(){
-        $(this).removeClass('animated fadeInUp ed-appear')
-        $(this).addClass('animated fadeOutDown');
+        $('.ed-magenta > .ed-home__button--menu--text').removeClass('animated fadeOutDown');
+        $('.ed-magenta > .ed-home__button--menu--text').addClass('animated fadeInUp');
+        $('.ed-magenta > .ed-home__button--menu--hover').removeClass('animated fadeInUp');
+        $('.ed-magenta > .ed-home__button--menu--hover').addClass('animated fadeOutDown');
+});
+
+$('.ed-home__button--menu.ed-yellow').mouseenter(
+    function(){
+        $('.ed-yellow > .ed-home__button--menu--text').addClass('animated fadeOutDown');
+        $('.ed-yellow > .ed-home__button--menu--hover').addClass('animated fadeInUp');
+        $('.ed-yellow > .ed-home__button--menu--hover').removeClass('fadeOutDown');
+}); 
+
+$('.ed-home__button--menu.ed-yellow').mouseleave(
+    function(){
+        $('.ed-yellow > .ed-home__button--menu--text').removeClass('animated fadeOutDown');
+        $('.ed-yellow > .ed-home__button--menu--text').addClass('animated fadeInUp');
+        $('.ed-yellow > .ed-home__button--menu--hover').removeClass('animated fadeInUp');
+        $('.ed-yellow > .ed-home__button--menu--hover').addClass('animated fadeOutDown');
+});
+
+$('.ed-home__button--menu.ed-black').mouseenter(
+    function(){
+        $('.ed-black > .ed-home__button--menu--text').addClass('animated fadeOutDown');
+        $('.ed-black > .ed-home__button--menu--hover').addClass('animated fadeInUp');
+        $('.ed-black > .ed-home__button--menu--hover').removeClass('fadeOutDown');
+}); 
+
+$('.ed-home__button--menu.ed-black').mouseleave(
+    function(){
+        $('.ed-black > .ed-home__button--menu--text').removeClass('animated fadeOutDown');
+        $('.ed-black > .ed-home__button--menu--text').addClass('animated fadeInUp');
+        $('.ed-black > .ed-home__button--menu--hover').removeClass('animated fadeInUp');
+        $('.ed-black > .ed-home__button--menu--hover').addClass('animated fadeOutDown');
 });
