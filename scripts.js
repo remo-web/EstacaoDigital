62d1943504881abd00ed488e132efb76223dfbaf
Raphael <raphaelrpais@gmail.com>
2018-01-30 17:13:20 -0200

    pretty fine

--- scripts.js
+++ scripts.js
@@ -7,18 +7,65 @@ $(function() {
         mdc.ripple.MDCRipple.attachTo(btn);
     }
     //tabs
-    mdc.tabs.MDCTabBar.attachTo(document.querySelector('#mdc-tab-bar'));
+//    mdc.tabs.MDCTabBar.attachTo(document.querySelector('#mdc-tab-bar'));
     //dropdown
     let menu = new mdc.menu.MDCSimpleMenu(document.querySelector('.mdc-simple-menu'));
     document.querySelector('.mdc-menu-anchor').addEventListener('click', () => menu.open = !menu.open);
+    
+    var dynamicTabBar = window.dynamicTabBar = new mdc.tabs.MDCTabBar(document.querySelector('#dynamic-tab-bar'));
+var dots = document.querySelector('.dots');
+var panels = document.querySelector('.panels');
 
+dynamicTabBar.tabs.forEach(function(tab) {
+  tab.preventDefaultOnClick = true;
 });
 
-//thumbs quadradas
+function updateDot(index) {
+  var activeDot = dots.querySelector('.dot.active');
+  if (activeDot) {
+    activeDot.classList.remove('active');
+  }
+  var newActiveDot = dots.querySelector('.dot:nth-child(' + (index + 1) + ')');
+  if (newActiveDot) {
+    newActiveDot.classList.add('active');
+  }
+}
 
-$(document).ready(function() {
-    var cw = $('figure.mdc-card').width();
-    $('figure.mdc-card').css({'height':cw+'px'});
+function updatePanel(index) {
+  var activePanel = panels.querySelector('.panel.active');
+  if (activePanel) {
+    activePanel.classList.remove('active');
+  }
+  var newActivePanel = panels.querySelector('.panel:nth-child(' + (index + 1) + ')');
+  if (newActivePanel) {
+    newActivePanel.classList.add('active');
+  }
+}
+
+dynamicTabBar.listen('MDCTabBar:change', function ({detail: tabs}) {
+  var nthChildIndex = tabs.activeTabIndex;
+
+  updatePanel(nthChildIndex);
+  updateDot(nthChildIndex);
+});
+
+dots.addEventListener('click', function (evt) {
+  if (!evt.target.classList.contains('dot')) {
+    return;
+  }
+
+  evt.preventDefault();
+
+  var dotIndex = [].slice.call(dots.querySelectorAll('.dot')).indexOf(evt.target);
+
+  if (dotIndex >= 0) {
+    dynamicTabBar.activeTabIndex = dotIndex;
+  }
+
+  updatePanel(dotIndex);
+  updateDot(dotIndex);
+})
+
 });
 
 //fullPage
@@ -27,8 +74,14 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
         anchors:['home', 'Lonas', 'Adesivos', 'Placas', 'Papel'],
         controlArrows: false,
-        scrollOverflow: true,
     });
+});
+
+//thumbs quadradas
+
+$(document).ready(function() {
+    var cw = $('.ed-gallery--card').width();
+    $('.ed-gallery--card').css({'height':cw+'px'});
 });
 
 //slimScroll
@@ -64,7 +117,7 @@ $(window).on('hashchange', function() {
         $('#ed-tab__servicos').addClass('mdc-tab--active');
         $('.mdc-tab-bar__indicator').css({ 'transform': "translateX(320px) scale(0.2, 1)" });
     }
-    if(/Servicos/.test(loc)) {
+    if(/Fizemos/.test(loc)) {
         $('#ed-tab__home').removeClass('mdc-tab--active');
         $('#ed-tab__quemsomos').removeClass('mdc-tab--active');
         $('#ed-tab__servicos').removeClass('mdc-tab--active');
