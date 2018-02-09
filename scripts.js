612c6909677d090d4cf923b96c6a9b9b33cf2e07
Raphael <raphaelrpais@gmail.com>
2018-02-09 15:53:04 -0200

    de volta a normalidade aparente
    início dos testes sem fullPage para navegação horizontal, utilizando as tabs do MDC

--- scripts.js
+++ scripts.js
@@ -7,65 +7,10 @@ $(function() {
         mdc.ripple.MDCRipple.attachTo(btn);
     }
     //tabs
-//    mdc.tabs.MDCTabBar.attachTo(document.querySelector('#mdc-tab-bar'));
+    mdc.tabs.MDCTabBar.attachTo(document.querySelector('#ed-header--tab'));
     //dropdown
-    let menu = new mdc.menu.MDCSimpleMenu(document.querySelector('.mdc-simple-menu'));
+    let menu = new mdc.menu.MDCMenu(document.querySelector('.mdc-menu'));
     document.querySelector('.mdc-menu-anchor').addEventListener('click', () => menu.open = !menu.open);
-//    
-//    var dynamicTabBar = window.dynamicTabBar = new mdc.tabs.MDCTabBar(document.querySelector('#dynamic-tab-bar'));
-//var dots = document.querySelector('.dots');
-//var panels = document.querySelector('.panels');
-//
-//dynamicTabBar.tabs.forEach(function(tab) {
-//  tab.preventDefaultOnClick = true;
-//});
-//
-//function updateDot(index) {
-//  var activeDot = dots.querySelector('.dot.active');
-//  if (activeDot) {
-//    activeDot.classList.remove('active');
-//  }
-//  var newActiveDot = dots.querySelector('.dot:nth-child(' + (index + 1) + ')');
-//  if (newActiveDot) {
-//    newActiveDot.classList.add('active');
-//  }
-//}
-//
-//function updatePanel(index) {
-//  var activePanel = panels.querySelector('.panel.active');
-//  if (activePanel) {
-//    activePanel.classList.remove('active');
-//  }
-//  var newActivePanel = panels.querySelector('.panel:nth-child(' + (index + 1) + ')');
-//  if (newActivePanel) {
-//    newActivePanel.classList.add('active');
-//  }
-//}
-//
-//dynamicTabBar.listen('MDCTabBar:change', function ({detail: tabs}) {
-//  var nthChildIndex = tabs.activeTabIndex;
-//
-//  updatePanel(nthChildIndex);
-//  updateDot(nthChildIndex);
-//});
-//
-//dots.addEventListener('click', function (evt) {
-//  if (!evt.target.classList.contains('dot')) {
-//    return;
-//  }
-//
-//  evt.preventDefault();
-//
-//  var dotIndex = [].slice.call(dots.querySelectorAll('.dot')).indexOf(evt.target);
-//
-//  if (dotIndex >= 0) {
-//    dynamicTabBar.activeTabIndex = dotIndex;
-//  }
-//
-//  updatePanel(dotIndex);
-//  updateDot(dotIndex);
-//})
-
 });
 
 //fullPage
