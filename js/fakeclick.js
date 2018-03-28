10294a8da8d28e09ed185d7c2ab9021e02d54e6d
Raphael <raphaelrpais@gmail.com>
2018-03-28 15:34:09 -0300

    navegacao e interacao
    melhorias

--- js/fakeclick.js
+++ js/fakeclick.js
@@ -1,14 +1,17 @@
 $(window).on('hashchange', function() {
+    if (window.location.hash.indexOf('') == 1) {
+        document.getElementById('ed-gohome').click();
+    }
     if (window.location.hash.indexOf('QuemSomos') == 1) {
-        document.getElementById('ed-quemsomos').click();
+        document.getElementById('ed-footer-quemsomos').click();
     }
     if (window.location.hash.indexOf('Servicos') == 1) {
-        document.getElementById('ed-servicos').click();
+        document.getElementById('ed-footer-servicos').click();
     }
     if (window.location.hash.indexOf('Fizemos') == 1) {
-        document.getElementById('ed-fizemos').click();
+        document.getElementById('ed-footer-fizemos').click();
     }
     if (window.location.hash.indexOf('Contato') == 1) {
-        document.getElementById('ed-contato').click();
+        document.getElementById('ed-footer-contato').click();
     }
 });
--- js/fullpage.js
+++ js/fullpage.js
@@ -5,5 +5,38 @@ $(document).ready(function() {
         keyboardScrolling: false,
         normalScrollElements:'.fp-kill',
         responsiveWidth: 840,
+        animateAnchor: false,
     });
+});
+
+//MAIN FOOTER
+$('#ed-footer-quemsomos').click(function(e){
+    $.fn.fullpage.silentMoveTo('home');
+});
+$('#ed-footer-servicos').click(function(e){
+    $.fn.fullpage.silentMoveTo('home');
+});
+$('#ed-footer-fizemos').click(function(e){
+    $.fn.fullpage.silentMoveTo('home');
+});
+$('#ed-footer-contato').click(function(e){
+    $.fn.fullpage.silentMoveTo('home');
+});
+
+//OUTROS FOOTER
+$('.ed-footer--toslide__lonas').click(function(e){
+    e.preventDefault();
+    $.fn.fullpage.silentMoveTo('Lonas');
+});
+$('.ed-footer--toslide__adesivos').click(function(e){
+    e.preventDefault();
+    $.fn.fullpage.silentMoveTo('Adesivos');
+});
+$('.ed-footer--toslide__placas').click(function(e){
+    e.preventDefault();
+    $.fn.fullpage.silentMoveTo('Placas');
+});
+$('.ed-footer--toslide__tecido').click(function(e){
+    e.preventDefault();
+    $.fn.fullpage.silentMoveTo('Tecido');
 });
