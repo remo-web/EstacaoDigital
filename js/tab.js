f0433e6fb914e35340dccd6dfb0279340a8b0c54
Raphael <raphaelrpais@gmail.com>
2018-08-07 00:32:24 -0300

    tab correct

--- js/tab.js
+++ js/tab.js
@@ -1,22 +1,30 @@
 function edTab() {
-    if (window.location.href("estacaodigitalrio.com.br") > -1) {
+    if (window.location.href.indexOf("estacaodigitalrio.com.br") > -1) {
         document.getElementById('ed-tab-home').classList.add('mdc-tab--active');
         document.getElementById('ed-indicator-home').classList.remove('ed-hide');
     }
     if (window.location.href.indexOf("QuemSomos") > -1) {
         document.getElementById('ed-tab-quemsomos').classList.add('mdc-tab--active');
         document.getElementById('ed-indicator-quemsomos').classList.remove('ed-hide');
+        document.getElementById('ed-tab-home').classList.remove('mdc-tab--active');
+        document.getElementById('ed-indicator-home').classList.add('ed-hide');
     }
     if (window.location.href.indexOf("Servicos") > -1) {
         document.getElementById('ed-tab-servicos').classList.add('mdc-tab--active');
         document.getElementById('ed-indicator-servicos').classList.remove('ed-hide');
+        document.getElementById('ed-tab-home').classList.remove('mdc-tab--active');
+        document.getElementById('ed-indicator-home').classList.add('ed-hide');
     }
     if (window.location.href.indexOf("Fizemos") > -1) {
         document.getElementById('ed-tab-fizemos').classList.add('mdc-tab--active');
         document.getElementById('ed-indicator-fizemos').classList.remove('ed-hide');
+        document.getElementById('ed-tab-home').classList.remove('mdc-tab--active');
+        document.getElementById('ed-indicator-home').classList.add('ed-hide');
     }
     if (window.location.href.indexOf("Contato") > -1) {
         document.getElementById('ed-tab-contato').classList.add('mdc-tab--active');
         document.getElementById('ed-indicator-contato').classList.remove('ed-hide');
+        document.getElementById('ed-tab-home').classList.remove('mdc-tab--active');
+        document.getElementById('ed-indicator-home').classList.add('ed-hide');
     }
 };
