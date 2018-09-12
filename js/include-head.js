c48a851377b4eb259721dde2bb44ef6115e0f32b
Raphael <raphaelrpais@gmail.com>
2018-09-12 12:14:54 -0300

    head test

--- js/include-head.js
+++ js/include-head.js
@@ -1,11 +1,11 @@
-function includeHTML() {
+function includeHEAD() {
   var z, i, elmnt, file, xhttp;
   /*loop through a collection of all HTML elements:*/
   z = document.getElementsByTagName("*");
   for (i = 0; i < z.length; i++) {
     elmnt = z[i];
     /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
+    file = elmnt.getAttribute("w3-include-head");
     if (file) {
       /*make an HTTP request using the attribute value as the file name:*/
       xhttp = new XMLHttpRequest();
@@ -14,7 +14,7 @@ function includeHTML() {
           if (this.status == 200) {elmnt.innerHTML = this.responseText;}
           if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
           /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("w3-include-html");
+          elmnt.removeAttribute("w3-include-head");
           includeHTML();
         }
       } 
@@ -24,7 +24,4 @@ function includeHTML() {
       return;
     }
   }
-    edMDC();
-    edTab();
-    initMap();
 }
